cad7518b69f8d19ef9cd0927142eeebda0f49c68
Astrid Augusta Yu <astridyu3.14@gmail.com>
2019-05-30 19:57:30 -0700

    we finally aren't crashing

--- train.py
+++ train.py
@@ -31,11 +31,22 @@ def _gen():
         
         yield input_resized, (output_transformer_tensor(output_hair), output_transformer_tensor(output_skin))
 
-dataset = tf.data.Dataset.from_generator(
-    _gen, 
-    (tf.float32, tf.float32),
-    ((224, 224, 3), (224, 224, 2))
-)
+def _img_map(input_filename, output_filename):
+    input_image = tf.read_file(input_filename)
+    input_image = tf.image.decode_jpeg(input_image, channels=3)
+    input_image = tf.image.resize_images(input_image, (224, 224))
+    input_image = tf.cast(input_image, tf.float32)
+
+    output_image = tf.read_file(output_filename)
+    output_image = tf.image.decode_bmp(output_image, channels=3)
+    output_image = tf.image.resize_images(output_image, (224, 224))
+    output_image = tf.cast(output_image, tf.float32)
+    output_image = output_image[:,:,1:2]
+
+    return input_image, output_image
+
+dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
+dataset = dataset.map(_img_map)
 dataset = dataset.batch(BATCH_SIZE)
 iterator = dataset.make_one_shot_iterator()
 images, labels = iterator.get_next()
